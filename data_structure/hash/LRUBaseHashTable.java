071db6828705e5189b57c4d93c67bf4d53019f18
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-09-05 15:56:14 +0800

    Update LRUBaseHashTable.java

--- data_structure/hash/LRUBaseHashTable.java
+++ data_structure/hash/LRUBaseHashTable.java
@@ -1,18 +1,16 @@
+package hash;
+
 import java.util.HashMap;
 
 /**
- * @Description:基于散列表的LRU算法
- * @Author: Hoda
- * @Date: Create in 2019-08-09
- * @Modified By:
- * @Modified Date:
+ * 基于散列表的LRU算法
  */
 public class LRUBaseHashTable<K, V> {
 
     /**
      * 默认链表容量
      */
-    private final static Integer DEFAULT_CAPACITY = 10;
+    private static final Integer DEFAULT_CAPACITY = 10;
 
     /**
      * 头结点
@@ -38,6 +36,120 @@ public class LRUBaseHashTable<K, V> {
      * 散列表存储key
      */
     private HashMap<K, DNode<K, V>> table;
+
+    public LRUBaseHashTable(int capacity) {
+        this.length = 0;
+        this.capacity = capacity;
+
+        headNode = new DNode<>();
+
+        tailNode = new DNode<>();
+
+        headNode.next = tailNode;
+        tailNode.prev = headNode;
+
+        table = new HashMap<>();
+    }
+
+    public LRUBaseHashTable() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * 新增 键值对
+     *
+     * @param key 键
+     * @param value 值
+     */
+    public void add(K key, V value) {
+        DNode<K, V> node = table.get(key);
+        if (node == null) {
+            DNode<K, V> newNode = new DNode<>(key, value);
+            table.put(key, newNode);
+            addNode(newNode);
+
+            if (++length > capacity) {
+                DNode<K, V> tail = popTail();
+                table.remove(tail.key);
+                length--;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    /**
+     * 将新节点加到头部
+     *
+     * @param newNode 新节点
+     */
+    private void addNode(DNode<K, V> newNode) {
+        newNode.next = headNode.next;
+        newNode.prev = headNode;
+
+        headNode.next.prev = newNode;
+        headNode.next = newNode;
+    }
+
+    /**
+     * 弹出尾部数据节点
+     */
+    private DNode<K, V> popTail() {
+        DNode<K, V> node = tailNode.prev;
+        removeNode(node);
+        return node;
+    }
+
+    /**
+     * 移除节点
+     *
+     * @param node 节点
+     */
+    private void removeNode(DNode<K, V> node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    /**
+     * 将节点移动到头部
+     *
+     * @param node 头结点
+     */
+    private void moveToHead(DNode<K, V> node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    /**
+     * 获取节点数据
+     *
+     * @param key 键
+     * @return 值
+     */
+    public V get(K key) {
+        DNode<K, V> node = table.get(key);
+        if (node == null) {
+            return null;
+        }
+        moveToHead(node);
+        return node.value;
+    }
+
+    /**
+     * 移除节点数据
+     *
+     * @param key 键
+     */
+    public void remove(K key) {
+        DNode<K, V> node = table.get(key);
+        if (node == null) {
+            return;
+        }
+        removeNode(node);
+        length--;
+        table.remove(node.key);
+    }
 
     /**
      * 双向链表
@@ -69,120 +181,6 @@ public class LRUBaseHashTable<K, V> {
             this.value = value;
         }
 
-    }
-
-    public LRUBaseHashTable(int capacity) {
-        this.length = 0;
-        this.capacity = capacity;
-
-        headNode = new DNode<>();
-
-        tailNode = new DNode<>();
-
-        headNode.next = tailNode;
-        tailNode.prev = headNode;
-
-        table = new HashMap<>();
-    }
-
-    public LRUBaseHashTable() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    /**
-     * 新增
-     *
-     * @param key
-     * @param value
-     */
-    public void add(K key, V value) {
-        DNode<K, V> node = table.get(key);
-        if (node == null) {
-            DNode<K, V> newNode = new DNode<>(key, value);
-            table.put(key, newNode);
-            addNode(newNode);
-
-            if (++length > capacity) {
-                DNode<K, V> tail = popTail();
-                table.remove(tail.key);
-                length--;
-            }
-        } else {
-            node.value = value;
-            moveToHead(node);
-        }
-    }
-
-    /**
-     * 将新节点加到头部
-     *
-     * @param newNode
-     */
-    private void addNode(DNode<K, V> newNode) {
-        newNode.next = headNode.next;
-        newNode.prev = headNode;
-
-        headNode.next.prev = newNode;
-        headNode.next = newNode;
-    }
-
-    /**
-     * 弹出尾部数据节点
-     */
-    private DNode<K, V> popTail() {
-        DNode<K, V> node = tailNode.prev;
-        removeNode(node);
-        return node;
-    }
-
-    /**
-     * 移除节点
-     *
-     * @param node
-     */
-    private void removeNode(DNode<K, V> node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    /**
-     * 将节点移动到头部
-     *
-     * @param node
-     */
-    private void moveToHead(DNode<K, V> node) {
-        removeNode(node);
-        addNode(node);
-    }
-
-    /**
-     * 获取节点数据
-     *
-     * @param key
-     * @return
-     */
-    public V get(K key) {
-        DNode<K, V> node = table.get(key);
-        if (node == null) {
-            return null;
-        }
-        moveToHead(node);
-        return node.value;
-    }
-
-    /**
-     * 移除节点数据
-     *
-     * @param key
-     */
-    public void remove(K key) {
-        DNode<K, V> node = table.get(key);
-        if (node == null) {
-            return;
-        }
-        removeNode(node);
-        length--;
-        table.remove(node.key);
     }
 
     private void printAll() {
