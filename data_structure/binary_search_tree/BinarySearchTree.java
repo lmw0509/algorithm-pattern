97ef3a4ba63aac0e01eaf764dd7f4f3b8ca15001
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-09-05 15:14:51 +0800

    Update BinarySearchTree.java

--- data_structure/binary_search_tree/BinarySearchTree.java
+++ data_structure/binary_search_tree/BinarySearchTree.java
@@ -1,22 +1,27 @@
+package binary_search_tree;
+
 public class BinarySearchTree {
     private Node tree;
 
-    public Node find(int data) {
+    public Node find(final int data) {
         Node p = tree;
         while (p != null) {
-            if (data < p.data) p = p.left;
-            else if (data > p.data) p = p.right;
-            else return p;
+            if (data < p.data) {
+                p = p.left;
+            } else if (data > p.data) {
+                p = p.right;
+            } else {
+                return p;
+            }
         }
         return null;
     }
 
-    public void insert(int data) {
+    public void insert(final int data) {
         if (tree == null) {
             tree = new Node(data);
             return;
         }
-
         Node p = tree;
         while (p != null) {
             if (data > p.data) {
@@ -25,7 +30,7 @@ public class BinarySearchTree {
                     return;
                 }
                 p = p.right;
-            } else { // data < p.data
+            } else {
                 if (p.left == null) {
                     p.left = new Node(data);
                     return;
@@ -35,42 +40,70 @@ public class BinarySearchTree {
         }
     }
 
-    public void delete(int data) {
-        Node p = tree; // p指向要删除的节点，初始化指向根节点
-        Node pp = null; // pp记录的是p的父节点
+    // 二叉搜索树稍微复杂，需要重点掌握
+    public void delete(final int data) {
+        // p指向要删除的节点，初始化指向根节点
+        Node p = tree;
+
+        // pp记录的是p的父节点
+        Node pp = null;
         while (p != null && p.data != data) {
             pp = p;
-            if (data > p.data) p = p.right;
-            else p = p.left;
+            if (data > p.data) {
+                p = p.right;
+            } else {
+                p = p.left;
+            }
         }
-        if (p == null) return; // 没有找到
+
+        // 没有找到
+        if (p == null) {
+            return;
+        }
 
         // 要删除的节点有两个子节点
-        if (p.left != null && p.right != null) { // 查找右子树中最小节点
+        if (p.left != null && p.right != null) {
+            // 查找右子树中最小节点
             Node minP = p.right;
-            Node minPP = p; // minPP表示minP的父节点
+            // minPP表示minP的父节点
+            Node minPP = p;
+
             while (minP.left != null) {
                 minPP = minP;
                 minP = minP.left;
             }
-            p.data = minP.data; // 将minP的数据替换到p中
-            p = minP; // 下面就变成了删除minP了
+
+            // 将minP的数据替换到p中
+            p.data = minP.data;
+
+            // 下面就变成了删除minP了
+            p = minP;
             pp = minPP;
         }
 
         // 删除节点是叶子节点或者仅有一个子节点
-        Node child; // p的子节点
-        if (p.left != null) child = p.left;
-        else if (p.right != null) child = p.right;
-        else child = null;
+        final Node child; // p的子节点
+        if (p.left != null) {
+            child = p.left;
+        } else if (p.right != null) {
+            child = p.right;
+        } else {
+            child = null;
+        }
 
-        if (pp == null) tree = child; // 删除的是根节点
-        else if (pp.left == p) pp.left = child;
-        else pp.right = child;
+        if (pp == null) {
+            tree = child; // 删除的是根节点
+        } else if (pp.left == p) {
+            pp.left = child;
+        } else {
+            pp.right = child;
+        }
     }
 
     public Node findMin() {
-        if (tree == null) return null;
+        if (tree == null) {
+            return null;
+        }
         Node p = tree;
         while (p.left != null) {
             p = p.left;
@@ -79,7 +112,9 @@ public class BinarySearchTree {
     }
 
     public Node findMax() {
-        if (tree == null) return null;
+        if (tree == null) {
+            return null;
+        }
         Node p = tree;
         while (p.right != null) {
             p = p.right;
@@ -92,7 +127,7 @@ public class BinarySearchTree {
         private Node left;
         private Node right;
 
-        public Node(int data) {
+        public Node(final int data) {
             this.data = data;
         }
     }
