d520c632daacd306a3df762fef751c8fd5087bb2
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-09-05 17:02:28 +0800

    Update StackBasedOnLinkedList.java

--- data_structure/stack_queue/StackBasedOnLinkedList.java
+++ data_structure/stack_queue/StackBasedOnLinkedList.java
@@ -1,54 +1,68 @@
-package stack;
+package stack_queue;
 
 /**
- * 基于链表实现的栈。
- *
- * Author: Zheng
+ * 基于链表实现的栈
  */
 public class StackBasedOnLinkedList {
-  private Node top = null;
+    private Node top = null;
 
-  public void push(int value) {
-    Node newNode = new Node(value, null);
-    // 判断是否栈空
-    if (top == null) {
-      top = newNode;
-    } else {
-      newNode.next = top;
-      top = newNode;
-    }
-  }
-
-  /**
-   * 我用-1表示栈中没有数据。
-   */
-  public int pop() {
-    if (top == null) return -1;
-    int value = top.data;
-    top = top.next;
-    return value;
-  }
-
-  public void printAll() {
-    Node p = top;
-    while (p != null) {
-      System.out.print(p.data + " ");
-      p = p.next;
-    }
-    System.out.println();
-  }
-
-  private static class Node {
-    private int data;
-    private Node next;
-
-    public Node(int data, Node next) {
-      this.data = data;
-      this.next = next;
+    public void push(int value) {
+        Node newNode = new Node(value, null);
+        // 判断是否栈空
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
     }
 
-    public int getData() {
-      return data;
+    /**
+     * 用-1表示栈中没有数据。
+     */
+    public int pop() {
+        if (top == null) {
+            return -1;
+        }
+        int value = top.data;
+        top = top.next;
+        return value;
     }
-  }
+
+    public void printAll() {
+        Node p = top;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+    }
+
+    public static void main(String[] args) {
+        StackBasedOnLinkedList stack = new StackBasedOnLinkedList();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
 }
