72f77b3957191b2fb783a0d0df8875287103645c
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-09-06 17:44:13 +0800

    Update Solution.java

--- src/com/lmw/leetcode/_0028/Solution.java
+++ src/com/lmw/leetcode/_0028/Solution.java
@@ -1,21 +1,27 @@
 package com.lmw.leetcode._0028;
 
-
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int l1 = haystack.length(), l2 = needle.length();
-        if (l1 < l2) return -1;
-        for (int i = 0; ; i++) {
-            if (i + l2 > l1) return -1;
-            for (int j = 0; ; j++) {
-                if (j == l2) return i;
-                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+    public int strStr(final String haystack, final String needle) {
+        final int l1 = haystack.length();
+        final int l2 = needle.length();
+        if (l1 < l2) {
+            return -1;
+        }
+        for (int i = 0; i < l1 - l2 + 1; i++) {
+            for (int j = 0; j < l2; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == l2 - 1) {
+                    return i;
+                }
             }
         }
+        return -1;
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
         System.out.println(solution.strStr("hello", "ll"));
     }
 }
