394c28bbd49506cb160a2fb7d53f3142e1258065
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-09-07 15:12:34 +0800

    Update Solution.java

--- src/com/lmw/leetcode/_0136/Solution.java
+++ src/com/lmw/leetcode/_0136/Solution.java
@@ -1,5 +1,6 @@
 package com.lmw.leetcode._0136;
 
+
 /**
  * Created with IntelliJ IDEA.
  * User: lmw
@@ -7,4 +8,17 @@ package com.lmw.leetcode._0136;
  * Description:
  */
 public class Solution {
+    public int singleNumber(final int[] nums) {
+        int result = 0;
+        for (final int num : nums) {
+            // 两个相同的数做异或操作等于0，0与任何数异或，数值不变。
+            result = result ^ num;
+        }
+        return result;
+    }
+
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[]{1,2,3,1,2}));
+    }
 }
