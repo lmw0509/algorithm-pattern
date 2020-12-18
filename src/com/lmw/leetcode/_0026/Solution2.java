1ae095d6a9b4718486dfb28384f28166577b9498
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-12-18 12:42:45 +0800

    Update Solution2.java

--- src/com/lmw/leetcode/_0026/Solution2.java
+++ src/com/lmw/leetcode/_0026/Solution2.java
@@ -1,1 +1,27 @@
+package com.lmw.leetcode._0026;
 
+/**
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shan-chu-pai-xu-shu-zu-zhong-de-zhong-fu-xiang-by-/
+ *
+ * @since 2020-12-18
+ */
+class Solution2 {
+    public int removeDuplicates(final int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(final String[] args) {
+        final Solution2 solution = new Solution2();
+        final int[] ints = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(solution.removeDuplicates(ints));
+    }
+}
