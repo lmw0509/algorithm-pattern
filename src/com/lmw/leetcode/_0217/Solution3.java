45e25a4330feae1fc72b6f8faf86286997002315
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-12-18 18:04:34 +0800

    Update Solution3.java

--- src/com/lmw/leetcode/_0217/Solution3.java
+++ src/com/lmw/leetcode/_0217/Solution3.java
@@ -1,1 +1,14 @@
+package com.lmw.leetcode._0217;
 
+import java.util.Arrays;
+
+/**
+ * 去重
+ *
+ * @since 2020-12-18
+ */
+public class Solution3 {
+    public boolean containsDuplicate(final int[] nums) {
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
+}
