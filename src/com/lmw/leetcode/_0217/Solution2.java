370c82860f999ebeeddcafbea9ce07bc72d2f59e
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-12-18 18:02:29 +0800

    Update Solution2.java

--- src/com/lmw/leetcode/_0217/Solution2.java
+++ src/com/lmw/leetcode/_0217/Solution2.java
@@ -1,1 +1,21 @@
+package com.lmw.leetcode._0217;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 哈希表
+ *
+ * @since 2020-12-18
+ */
+public class Solution2 {
+    public boolean containsDuplicate(final int[] nums) {
+        final Set<Integer> set = new HashSet<Integer>();
+        for (final int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
