7a5a00a954991dcb10021c4be4134e8a6f61b79c
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2021-03-08 19:43:27 +0800

    Create Solution.java

---
+++ src/com/lmw/leetcode/_0225/Solution.java
@@ -0,0 +1,50 @@
+package com.lmw.leetcode._0225;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://zhuanlan.zhihu.com/p/107761819
+ *
+ * @since 2021-03-08
+ */
+public class MyStack {
+    Queue<Integer> queue1;
+
+    Queue<Integer> queue2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+        queue1 = new LinkedList<Integer>();
+        queue2 = new LinkedList<Integer>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(final int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        final Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue1.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue1.peek();
+    }
+}
