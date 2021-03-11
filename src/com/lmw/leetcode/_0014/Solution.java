92b01ff204abf48caa8b0ef2ae8696ca8f6d385a
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2021-03-11 11:58:13 +0800

    Update Solution.java

--- src/com/lmw/leetcode/_0014/Solution.java
+++ src/com/lmw/leetcode/_0014/Solution.java
@@ -1,12 +1,15 @@
 package com.lmw.leetcode._0014;
 
-
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         int len = strs.length;
-        if (len == 0) return "";
-        int minLen = 0x7fffffff;
-        for (String str : strs) minLen = Math.min(minLen, str.length());
+        if (len == 0) {
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLen = Math.min(minLen, str.length());
+        }
         for (int j = 0; j < minLen; ++j)
             for (int i = 1; i < len; ++i)
                 if (strs[0].charAt(j) != strs[i].charAt(j))
@@ -14,10 +17,9 @@ class Solution {
         return strs[0].substring(0, minLen);
     }
 
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] strArr = new String[]{"flower", "flow", "flight"};
+        String[] strArr = new String[] {"flower", "flow", "flight"};
         System.out.println(solution.longestCommonPrefix(strArr));
     }
 }
